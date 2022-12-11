2c49e57062ea2f2d1b96031cfb7128ec0d55e2f7
tchstud22b4104 <120037291+tchstud22b4104@users.noreply.github.com>
2022-12-11 16:43:35 +0200

    Add files via upload

---
+++ Assets/Scripts/BuildingSystem.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+    public GameObject blockObject;
+
+    public Transform parent;
+
+    public Color normalColor;
+    public Color highlightedColor;
+
+    GameObject lastHightlightedBlock;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BuildBlock(blockObject);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            DestroyBlock();
+        }
+        HighlightBlock();
+    }
+
+    void BuildBlock(GameObject block)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2), Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
+
+            Instantiate(block, spawnPosition, Quaternion.identity, parent);
+        }
+    }
+
+    void DestroyBlock()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            if (hitInfo.transform.tag == "Block")
+            {
+                Destroy(hitInfo.transform.gameObject);
+            }
+        }
+    }
+
+    void HighlightBlock()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            if (hitInfo.transform.tag == "Block")
+            {
+                if (lastHightlightedBlock == null)
+                {
+                    lastHightlightedBlock = hitInfo.transform.gameObject;
+                    hitInfo.transform.gameObject.GetComponent<Renderer>().material.color = highlightedColor;
+                }
+                else if (lastHightlightedBlock != hitInfo.transform.gameObject)
+                {
+                    lastHightlightedBlock.GetComponent<Renderer>().material.color = normalColor;
+                    hitInfo.transform.gameObject.GetComponent<Renderer>().material.color = highlightedColor;
+                    lastHightlightedBlock = hitInfo.transform.gameObject;
+                }
+            }
+        }
+    }
+}
